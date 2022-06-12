a6ff3f8e0d2519091157615a069895e55e8e0fa4
Hang Shi <hangshiisi@gmail.com>
2022-06-12 11:04:50 -0700

    unit test python

---
+++ joy/tests/test_sum.py
@@ -0,0 +1,23 @@
+
+# Run under project directory 
+# python tests/test_sum.py 
+import sys
+sys.path.insert(0, "../joy")
+                
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
+
